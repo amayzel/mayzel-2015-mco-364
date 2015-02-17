d1e40fbfe1da8aeb07715e0aeb7080e315753bd0
amayzel <avigaylmayzel94@gmail.com>
2015-02-17 13:36:03 -0500

    resizing smiley
    Homework

--- src/Smile/SmileComponent.java
+++ src/Smile/SmileComponent.java
@@ -8,30 +8,39 @@ import javax.swing.JComponent;
 public class SmileComponent extends JComponent {
 
 	boolean wink = false;
-	int x = 120;
+	int width;
+	int height;
+	double x;
+	boolean go = true;
 	@Override
 	protected void paintComponent(Graphics g) {
 		super.paintComponent(g);
+		width = this.getWidth();
+		height = this.getHeight();
+		if(go){
+			x = height *.18;
+			go = false;
+		}
 		g.setColor(Color.YELLOW);
-		g.fillOval(190, 80, 400, 400);
+		g.fillOval((int) (width*.25), (int) (height*.15), (int) (width*.50), (int) (height*.70));
 		g.setColor(Color.BLUE);
-		g.fillOval(300, 150, 50, 50);
+		g.fillOval((int) (width*.38), (int) (height*.27), (int) (width*.07), (int) (height*.09));
 		g.setColor(Color.WHITE);
-		g.fillOval(330, 180, 15, 15);
+		g.fillOval((int) (width*.425), (int) (height*.32), (int) (width*.02), (int) (height*.027));
 		g.setColor(Color.BLUE);
-		g.fillOval(430, 150, 50, 50);
+		g.fillOval((int) (width*.543), (int) (height*.27), (int) (width*.07), (int) (height*.09));
 		g.setColor(Color.WHITE);
-		g.fillOval(460, 180, 15, 15);			
+		g.fillOval((int) (width*.588), (int) (height*.32), (int) (width*.02), (int) (height*.027));			
 		if(!wink){
 				g.setColor(Color.YELLOW);
-				g.fillOval(430, x++, 50, 50);
-			if(x == 150){
-				x= 90;
+				g.fillOval((int) (width*.543), (int) x++, (int) (width*.07), (int) (height*.09));
+			if(x >= height*.27){
+				x = height*.18;
 			}
 			wink = false;
 		}
 		g.setColor(Color.RED);
-		g.fillArc(290, 250, 200, 170, 0, -180);
+		g.fillArc((int) (width*.38), (int) (height*.45), (int) (width*.25), (int) (height*.32), 0, -180);
 	}
 
 }
