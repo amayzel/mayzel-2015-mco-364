1cf17522f82173594e230a7cc3c44ed56af0a653
amayzel <avigaylmayzel94@gmail.com>
2015-06-02 21:12:14 -0400

    paint done and physics done
    fixing

--- src/mayzel/physics/PhysicsFrame.java
+++ src/mayzel/physics/PhysicsFrame.java
@@ -8,19 +8,32 @@ public class PhysicsFrame extends JFrame{
 
 	public PhysicsFrame() {
 		setSize(400, 400);
-		setTitle("Body Frame");
+		setTitle("Physics Frame");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setLocationRelativeTo(null);
 		
-		Body body = new Body(new Force(0, 0), new Point2D.Double(0, 0));
+		final Body body = new Body(new Force(0, 0), new Point2D.Double(0, 0));
 		Force point = body.getForce();
 		PhysicsCanvas canvas = new PhysicsCanvas(point);
 		add(canvas);
-		while(true){
-			body.getForce().add(new Force(0, -9.8));
-			body.update();
-			repaint();
-		}
+		Thread t = new Thread() {
+
+			public void run() {
+				while (true) {
+					body.getForce().add(new Force(0, 9.8));
+					body.update();
+					repaint();
+					try {
+						Thread.sleep(700);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+				}
+			}
+		};
+
+		t.start();
+
 		
 	}
 	
